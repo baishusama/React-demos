affa356ae0226567db03aff53c0848bfcbb3ab75
baishusama <baishusama@outlook.com>
2016-11-28 17:19:35 +0800

    t
    staff_manage_system's basic logic is finished.

---
+++ staff_manage_system/app/StaffHeader.js
@@ -0,0 +1,51 @@
+import React, {
+    Component
+} from 'react';
+import {
+    findDOMNode
+} from 'react-dom'; // 
+
+class StaffHeader extends Component {
+    //search
+    handlerSearch() {
+        let bar = findDOMNode(this.refs.searchBar);
+        let value = bar.value;
+        this.props.searchStaff(value);
+    }
+
+    render() {
+        return (
+            <div>
+                <h3 style={{'textAlign':'center'}}>人员管理系统</h3>
+                <table className="optHeader">
+                    <tbody>
+                        <tr>
+                            <td className="headerTd">
+                                <input type="text" ref='searchBar' onChange={this.handlerSearch.bind(this)} placeholder="Search..." />
+                            </td>
+                            <td className="headerTd">
+                                <label htmlFor="idSelect">人员筛选</label>
+                                <select name="idSelect" id="idSelect">
+                                    <option value="0">主任</option>
+                                    <option value="1">老师</option>
+                                    <option value="2">学生</option>
+                                    <option value="3">实习</option>
+                                </select>
+                            </td>
+                            <td className="headerTd">
+                                <label htmlFor="orderSelect">排列方式</label>
+                                <select name="orderSelect" id="orderSelect">
+                                    <option value="0">身份</option>
+                                    <option value="1">年龄 升</option>
+                                    <option value="2">年龄 降</option>
+                                </select>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+export default StaffHeader
---
+++ staff_manage_system/app/StaffItem.js
@@ -0,0 +1,22 @@
+import React, {
+    Component
+} from 'react';
+
+class StaffItem extends Component {
+    render() {
+        return (
+            <tr style={{'cursor': 'pointer'}}>
+                <td className="itemTd">{this.props.item.info.name}</td>
+                <td className="itemTd">{this.props.item.info.age}</td>
+                <td className="itemTd">{this.props.item.info.id}</td>
+                <td className="itemTd">{this.props.item.info.sex}</td>
+                <td className="itemTd">
+                    <a href="#" className="itemBtn">删除</a>
+                    <a href="#" className="itemBtn">详情</a>
+                </td>
+            </tr>
+        );
+    }
+}
+
+export default StaffItem
